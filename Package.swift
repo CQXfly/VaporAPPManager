1203bc91ad496b13abfb3dea992a19d00b41ebfb
qingxu.chong <qingxu.chong@yintech.cn>
2018-07-19 19:39:41 +0800

    添加qxtcpserver

--- Package.swift
+++ Package.swift
@@ -9,10 +9,11 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
 //        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.4.0.1")
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.4.0.1"),
+        .package(url: "https://github.com/CQXfly/QXTCPServer_Vapor", from: "0.3.1"),
     ],
     targets: [
-        .target(name: "App", dependencies: [ "Vapor","FluentMySQL",]),
+        .target(name: "App", dependencies: [ "Vapor","FluentMySQL","QXTCPServer"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,17 @@
 import Vapor
-
+import QXTCPServer
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
+    let tcp = TCPServer(host: "127.0.0.1", port: 3000, eventLoop: app.eventLoop);
+    tcp.onRead = { x, y in
+        var z = ByteBufferAllocator().buffer(capacity: 20)
+        z.write(string: "123")
+        x.write(z).mapIfError({ (e) in
+            print(e)
+        })
+        
+        
+    }
+    tcp.listen()
 }
